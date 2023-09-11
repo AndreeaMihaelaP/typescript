779a102cb4ca94384c2f35ce0c0f4f75e1ff15c6
AndreeaMihaelaP <andreeapatrascu1997@gmail.com>
2023-09-11 09:04:34 +0100

    typescript theory

---
+++ typescript-legacy/fetch-json/index.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  // Response.data has properties of
+  // id
+  // title
+  // completed
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+  The Todo with id: ${id}
+  Has a title of: ${title}
+  Is it finished? ${completed}`);
+};
