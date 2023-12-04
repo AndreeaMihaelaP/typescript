23c4068ca139091b82df0b107763f65187a30b70
AndreeaMihaelaP <andreeapatrascu1997@gmail.com>
2023-12-04 07:45:10 +0000

    ➕ add: class info

--- typescript-new/demo/classes/classes.ts
+++ typescript-new/demo/classes/classes.ts
@@ -8,3 +8,26 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/classes.html
+
+class Color {
+  r: number = 0;
+  g: number = 0;
+  b: number = 0;
+}
+
+const red = new Color();
+red.r = 255;
+
+const redValue = red.r;
+
+class Dimension {
+  length: number;
+  width: number;
+  height: number;
+
+  constructor() {
+    this.length = 1;
+    this.width = 1;
+    this.height = 1;
+  }
+}
